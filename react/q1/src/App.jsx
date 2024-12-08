0e7c4d634044261353523ccf8b7d5504731a88df
PoojaSingh31github <pooja.singh.masaischool@gmail.com>
2024-12-08 17:52:51 +0530

    conplieted

--- react/q1/src/App.jsx
+++ react/q1/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import EditProfile from "./components/editProfile";
 import UserProfile from "./components/userProfile";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [isEditMode, setIsEditMode] = useState(false);
@@ -15,14 +17,17 @@ function App() {
     setIsEditMode(!isEditMode);
   };
 
+  const notify = () => toast("user updated successfully");
   // Update user data
   const updateUserData = (updatedData) => {
     setUserData(updatedData); // Update parent state
+    notify()
     setIsEditMode(false); // Switch back to View Mode
   };
 
   return (
     <div>
+      <ToastContainer />
       <button onClick={toggleEditMode}>
         {isEditMode ? "View Profile" : "Edit Profile"}
       </button>
